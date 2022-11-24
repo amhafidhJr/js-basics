84195e705f99e860b9c3a5c730b5cd86cb9664b0
amhafidhJr <abdulhalimhafidh5@gmail.com>
2022-11-24 23:28:40 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -7,21 +7,25 @@
 
 // sayHello();
 
-const person = {
-  name: "Abdulhalim",
-  age: 22,
-  address: "123 Main Street",
-  education () {
-    const edu_background = this;
-    console.log(edu_background);
-    // console.log("Degree of Mathematics");
-  },
-};
+// const person = {
+//   name: "Abdulhalim",
+//   age: 22,
+//   address: "123 Main Street",
+//   education () {
+//     const edu_background = this;
+//     console.log(edu_background);
+//     // console.log("Degree of Mathematics");
+//   },
+// };
 
-// console.log(person);
-// //change person name to "Mohamed"
-// person.name = "Mohamed";
-// console.log(person.education());
+// // console.log(person);
+// // //change person name to "Mohamed"
+// // person.name = "Mohamed";
+// // console.log(person.education());
 
-const education = person.education.bind(person);
-education();
+// const education = person.education.bind(person);
+// education();
+
+const colors = ["blue", "green", "yellow", "red"];
+const items = colors.map((color) => `<li><b>${color}</b></li>`);
+console.log(items);
